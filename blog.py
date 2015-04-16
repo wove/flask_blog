49041ad87fff3c03074839372179836453c11aa9
Tomlin Leniston <leniston@gmail.com>
2015-04-16 12:50:20 +1000

    initial controller config

---
+++ blog.py
@@ -0,0 +1,20 @@
+# -- Blog controller -- #
+
+
+from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+import sqlite3
+
+# Configuration
+DATABASE = 'blog.db'
+
+app = Flask(__name__)
+
+# pulls in app configuration by looking for UPPERCASE variables
+app.config.from_object(__name__)
+
+# Function for connecting to the database
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
