469f789fbb22cb57b3d6878390eac7705740cc0e
Tomlin Leniston <leniston@gmail.com>
2015-04-17 11:43:53 +1000

    added basic user authentication

--- blog.py
+++ blog.py
@@ -1,11 +1,14 @@
 # -- Blog controller -- #
 
-
+import os
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 import sqlite3
 
 # Configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
 
 app = Flask(__name__)
 
@@ -16,9 +19,22 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+            error = "Invalid credentials. Please try again."
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 @app.route('/main')
 def main():
