f19f547142fa82e5c084c3a79e94075d53544369
Tomlin Leniston <leniston@gmail.com>
2015-04-17 12:51:47 +1000

    added css and post submission

--- blog.py
+++ blog.py
@@ -3,6 +3,7 @@
 import os
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
 # Configuration
 DATABASE = 'blog.db'
@@ -19,6 +20,19 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+# A login required decorator
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+        
+    return wrap
+
+# View for login page
 @app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
@@ -30,15 +44,40 @@ def login():
             return redirect(url_for('main'))
     return render_template('login.html', error=error)
 
+# View for logged out page (redirects to login view once complete)
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('login'))
 
-@app.route('/main')
-def main():
-    return render_template('main.html')
+# View for adding posts
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash('All fields are required. Please try again')
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute("INSERT INTO posts (title, post) VALUES(?, ?)", [title, post])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully added')
+        return redirect(url_for('main'))
 
+# View for main page of blog
+@app.route('/main')
+@login_required
+def main():
+    g.db = connect_db()
+    cur = g.db.execute("SELECT * FROM posts")
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
+
+# Runs app if called directly (i.e. not if imported by another module)
 if __name__ == '__main__':
     app.run(debug=True)
